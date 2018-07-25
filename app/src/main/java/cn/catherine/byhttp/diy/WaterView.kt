8cc25cf7261d16d627d6785b58c23490c7650508
Catherine.Brain William <15520054289@163.com>
2018-07-25 16:20:18 +0800

    review

---
+++ app/src/main/java/cn/catherine/byhttp/diy/WaterView.kt
@@ -0,0 +1,17 @@
+package cn.catherine.byhttp.diy
+
+import android.content.Context
+import android.view.View
+
+/**
+ *
+ * ByHttp
+ *
+ * cn.catherine.byhttp.diy
+ *
+ * created by catherine in 七月/25/2018/下午4:01
+ */
+
+class WaterView(context: Context?) : View(context) {
+
+}
